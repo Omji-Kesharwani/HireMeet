f61f1c75cd22108548c9cac808d2dffe502e9675
Omji-Kesharwani <kesharwaniomji000@gmail.com>
2026-01-26 20:49:48 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import { ENV } from "./lib/env.js";
+import cors from "cors";
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://hiremeet.vercel.app"
+    ],
+    credentials: true
+  })
+);
+dotenv.config();
+
+app.get("/health", (req, res) => {
+  res.status(200).json({msg:"Server is running and up!"});
+})
+app.get("/book", (req, res) => {
+  res.status(200).json({msg:"Hello books"});
+})
+
+app.listen(ENV.PORT, () => {
+  console.log(`Server is running on port ${ENV.PORT}`);
+});

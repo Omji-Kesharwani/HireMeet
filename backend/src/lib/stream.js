d95e016ab9e8117b615725d124a0201e69934540
Omji-Kesharwani <kesharwaniomji000@gmail.com>
2026-01-27 22:03:23 +0530

    bug fixing

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,7 +1,7 @@
 import {StreamChat} from "stream-chat";
-import { ENV } from "../lib/env.js";
+import { ENV } from "./env.js";
 
-const apiKey  = ENV.STREAM_API_KEY;
+const apiKey = ENV.STREAM_API_KEY;
 const apiSecret = ENV.STREAM_API_SECRET;
 
 if(!apiKey || !apiSecret){

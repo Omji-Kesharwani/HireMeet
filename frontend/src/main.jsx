0deba387d916c64e308bb0a3f59053b53ad65a30
Omji-Kesharwani <kesharwaniomji000@gmail.com>
2026-01-28 22:06:28 +0530

    navbar ui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,34 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ClerkProvider } from '@clerk/clerk-react'
-import { BrowserRouter } from 'react-router'
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Clerk Publishable Key')
+  throw new Error("Missing Clerk Publishable Key");
 }
-  const queryClient = useQueryClient()
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-     <BrowserRouter>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
         <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
           <App />
-      </ClerkProvider>
+        </ClerkProvider>
+      </QueryClientProvider>
     </BrowserRouter>
-    </QueryClientProvider>
-    
-   
-  </StrictMode>,
-)
+  </StrictMode>
+);

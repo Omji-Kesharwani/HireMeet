971960f9228ca1df8bef158a1bec734d21a70c9c
Omji-Kesharwani <kesharwaniomji000@gmail.com>
2026-01-28 19:31:28 +0530

    frontend tools setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,17 +3,32 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { BrowserRouter } from 'react-router'
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Clerk Publishable Key')
 }
+  const queryClient = useQueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-        <App />
-    </ClerkProvider>
+    <QueryClientProvider client={queryClient}>
+     <BrowserRouter>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+          <App />
+      </ClerkProvider>
+    </BrowserRouter>
+    </QueryClientProvider>
+    
+   
   </StrictMode>,
 )
